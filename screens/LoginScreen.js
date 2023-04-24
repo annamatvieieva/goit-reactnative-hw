c6313f17a7cb9a260616aae7d60d4b003f47c8b1
Anna Matvieieva <90278772+annamatvieieva@users.noreply.github.com>
2023-04-24 22:33:12 +0300

    finished hw2

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,36 +1,125 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import {
   StyleSheet,
   Text,
   View,
   ImageBackground,
   TextInput,
-  Button,
-	Image,
-  TouchableOpacity
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 
-const backgroundImage = require('../images/backgroundImage.jpg');
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+const backgroundImage = require("../images/backgroundImage.jpg");
+
+SplashScreen.preventAutoHideAsync();
 
 export function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [hide, setHide] = useState(true);
+  const [focusInput, setFocusInput] = useState("");
+
+  const [fontsLoaded] = useFonts({
+    Roboto: require("../assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  const hidePassword = () => {
+    if (!hide) {
+      setHide(true);
+    } else {
+      setHide(false);
+    }
+  };
+
+  const hideKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
+  const handleSubmit = () => {
+    const user = {
+      email,
+      password,
+    };
+    console.log(user);
+  };
+
   return (
-    <View style={styles.container}>
-      <ImageBackground style={styles.image} source={backgroundImage} resizeMode="cover">
-        <View style={styles.form}>
-          <Text style={styles.title}>Войти</Text>
-          <TextInput  style={styles.input} placeholder="Адрес электронной почты" />
-          <View style={styles.inputContainer}>
-            <TextInput style={[styles.input, styles.inputPassword,]}  placeholder="Пароль" />
-            <TouchableOpacity style={styles.buttonPassword}>
-              <Text style={styles.buttonPasswordTitle}>Показать</Text>
-            </TouchableOpacity>
-          </View>
-          <TouchableOpacity style={styles.button}>
-              <Text style={styles.buttonTitle}>Войти</Text>
-            </TouchableOpacity>
-					<Text style={styles.link}>Нет аккаунта? Зарегистрироваться</Text>
-        </View>
-      </ImageBackground>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <TouchableWithoutFeedback onPress={hideKeyboard}>
+        <ImageBackground
+          style={styles.image}
+          source={backgroundImage}
+          resizeMode="cover"
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={-241}
+          >
+            <View style={{ ...styles.form, fontFamily: "Roboto" }}>
+              <Text style={styles.title}>Войти</Text>
+              <TextInput
+                style={{
+                  ...styles.input,
+                  borderColor:
+                    focusInput === "emailInput" ? "#FF6C00" : "#E8E8E8",
+                }}
+                placeholder="Адрес электронной почты"
+                value={email}
+                maxLength={30}
+                keyboardType="email-address"
+                onChangeText={(text) => setEmail(text)}
+                onFocus={() => setFocusInput("emailInput")}
+                onBlur={() => setFocusInput("")}
+              />
+              <View style={styles.inputContainer}>
+                <TextInput
+                  style={{
+                    ...styles.input,
+                    ...styles.inputPassword,
+                    borderColor:
+                      focusInput === "passwordInput" ? "#FF6C00" : "#E8E8E8",
+                  }}
+                  placeholder="Пароль"
+                  secureTextEntry={hide}
+                  value={password}
+                  maxLength={30}
+                  onChangeText={(text) => setPassword(text)}
+                  onFocus={() => setFocusInput("passwordInput")}
+                  onBlur={() => setFocusInput("")}
+                />
+                <TouchableOpacity
+                  style={styles.buttonPassword}
+                  onPress={hidePassword}
+                >
+                  <Text style={styles.buttonPasswordTitle}>
+                    {hide ? "Показать" : "Скрыть"}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+                <Text style={styles.buttonTitle}>Войти</Text>
+              </TouchableOpacity>
+              <Text style={styles.link}>Нет аккаунта? Зарегистрироваться</Text>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
@@ -38,61 +127,41 @@ export function LoginScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '##F6F6F6',
+    backgroundColor: "##F6F6F6",
   },
-	image: {
-		flex: 1,
-		justifyContent: 'flex-end',
-	},
-	form: {
+  image: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  form: {
     minHeight: 489,
-    alignItems: 'center',
-		backgroundColor: '#FFFFFF',
-		paddingHorizontal: 16,
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+    paddingHorizontal: 16,
     paddingTop: 32,
     paddingBottom: 144,
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-    position: 'relative',
-    // fontFamily: 'Roboto',
-   fontStyle: 'normal',
-   fontWeight: '400',
-   fontSize: 16,
-   lineHeight: 19,
-	},
-	avatarContainer: {
-		width: 120,
-    height: 120,
-		backgroundColor: '#F6F6F6',
-		borderRadius: 16,
-    position: 'absolute',
-    top: -60,
-	},
-	buttonAddAvatar: {
-		width: 25,
-		height: 25,
-		backgroundColor:'#FFFFFF',
-		borderRadius: 12.5,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    right: -12.5,
-    bottom: 14,
-	},
+    position: "relative",
+    fontStyle: "normal",
+    fontWeight: "400",
+    fontSize: 16,
+    lineHeight: 19,
+  },
   title: {
-    fontWeight: '500',
+    fontWeight: "500",
     fontSize: 30,
     lineHeight: 35,
     letterSpacing: 0.01,
-    color: '#212121',
+    color: "#212121",
     marginBottom: 33,
-  }, 
+  },
   input: {
     height: 50,
-    width: '100%',
-    backgroundColor: '#F6F6F6',
+    width: "100%",
+    backgroundColor: "#F6F6F6",
     borderWidth: 1,
-    borderColor: '#E8E8E8',
+    borderColor: "#E8E8E8",
     borderRadius: 8,
     paddingHorizontal: 16,
     marginBottom: 16,
@@ -102,32 +171,32 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     height: 50,
-    width: '100%',
-    position: 'relative',
+    width: "100%",
+    position: "relative",
     borderRadius: 8,
     marginBottom: 43,
   },
   buttonPassword: {
-    position: 'absolute',
+    position: "absolute",
     right: 16,
     bottom: 16,
   },
   buttonPasswordTitle: {
-    color: '#1B4371',
+    color: "#1B4371",
   },
   button: {
     height: 50,
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FF6C00',
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FF6C00",
     borderRadius: 100,
     marginBottom: 16,
   },
   buttonTitle: {
-   color: '#FFFFFF',
+    color: "#FFFFFF",
   },
   link: {
-   color: '#1B4371',
-  }
+    color: "#1B4371",
+  },
 });
--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -1,44 +1,185 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import {
   StyleSheet,
   Text,
   View,
   ImageBackground,
   TextInput,
-  Button,
-	Image,
-  TouchableOpacity
+  Image,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 
-const backgroundImage = require('../images/backgroundImage.jpg');
-const plusIcon = require('../images/add.png');
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+import * as ImagePicker from "expo-image-picker";
+
+const backgroundImage = require("../images/backgroundImage.jpg");
+const plusIcon = require("../images/plus.png");
+const closeIcon = require("../images/close.png");
+
+SplashScreen.preventAutoHideAsync();
 
 export function RegistrationScreen() {
+  const [avatar, setAvatar] = useState(null);
+  const [login, setLogin] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [hide, setHide] = useState(true);
+  const [focusInput, setFocusInput] = useState("");
+
+  const [fontsLoaded] = useFonts({
+    Roboto: require("../assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  const pickImage = async () => {
+    if (avatar) {
+      setTimeout(() => setAvatar(null), 2000);
+    } else {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      console.log(result);
+
+      if (!result.canceled) {
+        setAvatar(result.assets[0].uri);
+      }
+    }
+  };
+
+  const hidePassword = () => {
+    if (!hide) {
+      setHide(true);
+    } else {
+      setHide(false);
+    }
+  };
+
+  const hideKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
+  const handleSubmit = () => {
+    const user = {
+      avatar,
+      login,
+      email,
+      password,
+    };
+    console.log(user);
+  };
+
   return (
-    <View style={styles.container}>
-      <ImageBackground style={styles.image} source={backgroundImage} resizeMode="cover">
-        <View style={styles.form}>
-          <View style={styles.avatarContainer}>
-						<Image resizeMode="contain"/>
-            <TouchableOpacity style={styles.buttonAddAvatar}>
-              <Image source={plusIcon}/>
-            </TouchableOpacity>
-          </View>
-          <Text style={styles.title}>Регистрация</Text>
-          <TextInput style={styles.input} placeholder="Логин" />
-          <TextInput  style={styles.input} placeholder="Адрес электронной почты" />
-          <View style={styles.inputContainer}>
-            <TextInput style={[styles.input, styles.inputPassword,]}  placeholder="Пароль" />
-            <TouchableOpacity style={styles.buttonPassword}>
-              <Text style={styles.buttonPasswordTitle}>Показать</Text>
-            </TouchableOpacity>
-          </View>
-          <TouchableOpacity style={styles.button}>
-              <Text style={styles.buttonTitle}>Зарегистрироваться</Text>
-            </TouchableOpacity>
-					<Text style={styles.link}>Уже есть аккаунт? Войти</Text>
-        </View>
-      </ImageBackground>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <TouchableWithoutFeedback onPress={hideKeyboard}>
+        <ImageBackground
+          style={styles.image}
+          source={backgroundImage}
+          resizeMode="cover"
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={-175}
+          >
+            <View style={{ ...styles.form, fontFamily: "Roboto" }}>
+              <View style={styles.avatarContainer}>
+                {avatar && (
+                  <Image
+                    resizeMode="contain"
+                    source={{ uri: avatar }}
+                    style={{ width: 120, height: 120, borderRadius: 16}}
+                  />
+                )}
+                <TouchableOpacity
+                  style={{
+                    ...styles.buttonAddAvatar,
+                    borderColor: avatar ? "#E8E8E8" : "#FF6C00",
+                  }}
+                  onPress={pickImage}
+                >
+                  {avatar && <Image source={closeIcon} />}
+                  {!avatar && <Image source={plusIcon} />}
+                </TouchableOpacity>
+              </View>
+              <Text style={styles.title}>Регистрация</Text>
+              <TextInput
+                style={{
+                  ...styles.input,
+                  borderColor:
+                    focusInput === "loginInput" ? "#FF6C00" : "#E8E8E8",
+                }}
+                placeholder="Логин"
+                value={login}
+                maxLength={30}
+                onChangeText={(text) => setLogin(text)}
+                onFocus={() => setFocusInput("loginInput")}
+                onBlur={() => setFocusInput("")}
+              />
+              <TextInput
+                style={{
+                  ...styles.input,
+                  borderColor:
+                    focusInput === "emailInput" ? "#FF6C00" : "#E8E8E8",
+                }}
+                placeholder="Адрес электронной почты"
+                value={email}
+                maxLength={30}
+                keyboardType="email-address"
+                onChangeText={(text) => setEmail(text)}
+                onFocus={() => setFocusInput("emailInput")}
+                onBlur={() => setFocusInput("")}
+              />
+              <View style={styles.inputContainer}>
+                <TextInput
+                  style={{
+                    ...styles.input,
+                    ...styles.inputPassword,
+                    borderColor:
+                      focusInput === "passwordInput" ? "#FF6C00" : "#E8E8E8",
+                  }}
+                  placeholder="Пароль"
+                  secureTextEntry={hide}
+                  value={password}
+                  maxLength={30}
+                  onChangeText={(text) => setPassword(text)}
+                  onFocus={() => setFocusInput("passwordInput")}
+                  onBlur={() => setFocusInput("")}
+                />
+                <TouchableOpacity
+                  style={styles.buttonPassword}
+                  onPress={hidePassword}
+                >
+                  <Text style={styles.buttonPasswordTitle}>
+                    {hide ? "Показать" : "Скрыть"}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+                <Text style={styles.buttonTitle}>Зарегистрироваться</Text>
+              </TouchableOpacity>
+              <Text style={styles.link}>Уже есть аккаунт? Войти</Text>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
@@ -46,96 +187,101 @@ export function RegistrationScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '##F6F6F6',
+    backgroundColor: "##F6F6F6",
   },
-	image: {
-		flex: 1,
-		justifyContent: 'flex-end',
-	},
-	form: {
+  image: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  form: {
     minHeight: 549,
-    alignItems: 'center',
-		backgroundColor: '#FFFFFF',
-		paddingHorizontal: 16,
-    paddingTop: 92,
+    width: "100%",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+    paddingHorizontal: 16,
     paddingBottom: 78,
+    paddingTop: 92,
+    position: "relative",
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-    position: 'relative',
-    // fontFamily: 'Roboto',
-   fontStyle: 'normal',
-   fontWeight: '400',
-   fontSize: 16,
-   lineHeight: 19,
-	},
-	avatarContainer: {
-		width: 120,
+    fontStyle: "normal",
+    fontWeight: "400",
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  avatarContainer: {
+    width: 120,
     height: 120,
-		backgroundColor: '#F6F6F6',
-		borderRadius: 16,
-    position: 'absolute',
+    backgroundColor: "#F6F6F6",
+    borderRadius: 16,
+    position: "absolute",
     top: -60,
-	},
-	buttonAddAvatar: {
-		width: 25,
-		height: 25,
-		backgroundColor:'#FFFFFF',
-		borderRadius: 12.5,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
+  },
+  buttonAddAvatar: {
+    width: 25,
+    height: 25,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 12.5,
+    borderWidth: 1,
+    borderStyle: "solid",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
     right: -12.5,
     bottom: 14,
-	},
+  },
   title: {
-    fontWeight: '500',
+    fontWeight: "500",
     fontSize: 30,
     lineHeight: 35,
     letterSpacing: 0.01,
-    color: '#212121',
+    color: "#212121",
     marginBottom: 33,
-  }, 
+  },
   input: {
     height: 50,
-    width: '100%',
-    backgroundColor: '#F6F6F6',
+    width: "100%",
+    backgroundColor: "#F6F6F6",
     borderWidth: 1,
-    borderColor: '#E8E8E8',
+    borderColor: "#E8E8E8",
     borderRadius: 8,
+    borderStyle: "solid",
     paddingHorizontal: 16,
     marginBottom: 16,
+  },
+  inputContainer: {
+    height: 50,
+    width: "100%",
+    position: "relative",
+    borderRadius: 8,
+    marginBottom: 43,
   },
   inputPassword: {
     marginBottom: 0,
   },
-  inputContainer: {
-    height: 50,
-    width: '100%',
-    position: 'relative',
-    borderRadius: 8,
-    marginBottom: 43,
-  },
   buttonPassword: {
-    position: 'absolute',
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
     right: 16,
-    bottom: 16,
   },
   buttonPasswordTitle: {
-    color: '#1B4371',
+    color: "#1B4371",
   },
   button: {
     height: 50,
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FF6C00',
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FF6C00",
     borderRadius: 100,
     marginBottom: 16,
   },
   buttonTitle: {
-   color: '#FFFFFF',
+    color: "#FFFFFF",
   },
   link: {
-   color: '#1B4371',
-  }
+    color: "#1B4371",
+  },
 });
